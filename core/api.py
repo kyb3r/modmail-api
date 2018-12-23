859c69d488ec83f8d8ebf1cd21aff3f54db012f6
verixx <abdurraqeeb53@gmail.com>
2018-12-23 14:34:10 +1100

    Update api.py

--- core/api.py
+++ core/api.py
@@ -22,7 +22,7 @@ async def upgrade(request):
     request.app.loop.create_task(restart_later(request.app))
     return response.json({'success': True})
 
-@api.get('/modmail/meta')
+@api.get('/modmail')
 async def get_modmail_info(request):
     app = request.app
 
