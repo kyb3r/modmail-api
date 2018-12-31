12fc9d1465dddf714eed58880256d6c22f3a4aca
kyb3r <abdurraqeeb53@gmail.com>
2018-12-31 18:07:17 +1100

    Fix logs url prefix

--- core/logs.py
+++ core/logs.py
@@ -3,10 +3,14 @@ from core import config
 
 domain = config.DOMAIN
 host = None if config.DEV_MODE else f'logs.{domain}'
-prefix = '/logs/' if config.DEV_MODE else '/'
+prefix = '/logs' if config.DEV_MODE else None
 
 logs = Blueprint('logs', host=host, url_prefix=prefix)
+    
+@logs.get('/')
+async def index(request):
+    return response.text('logs')
 
-logs.get('/<guild_id>/<key>')
+@logs.get('/<guild_id>/<key>')
 async def getlogsfile(request, guild_id, key):
     return response.json({'message': 'not implemented'})
