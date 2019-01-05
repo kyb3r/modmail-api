288db440d36a86a33d2978d827c0f04f2ce99310
kyb3r <abdurraqeeb53@gmail.com>
2019-01-05 16:07:11 +1100

    Fix setting

--- core/api.py
+++ core/api.py
@@ -149,7 +149,8 @@ async def get_config(request, auth_info):
 async def update_config(request, auth_info):
     user_id = auth_info['user_id']
 
-    to_set = {k: v for k, v in request.json.items() if v}
+    to_set = {f'config.{k}': v for k, v in request.json.items() if v}
+    
     unset = {f'config.{k}': 1 for k, v in request.json.items() if not v}
 
     operations = {'$set': {'config': to_set}}
